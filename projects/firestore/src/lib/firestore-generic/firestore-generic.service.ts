793c11a9f7f06ac72638d8c6d5bf9aedbe4ee1e5
Chris Curnow <chris@4cast.com.au>
2020-11-18 10:26:33 +1100

    feat: add dummy method to firestore-generic just to trigger release

--- projects/firestore/src/lib/firestore-generic/firestore-generic.service.ts
+++ projects/firestore/src/lib/firestore-generic/firestore-generic.service.ts
@@ -14,6 +14,17 @@ export class FirestoreGenericService {
 addByPath(collectionPath: string, data: any): Promise<void> {
   const collection = this.afs.collection(collectionPath);
   const id = data.id;
-  return collection.doc(id).set(data); }
+  return collection.doc(id).set(data);
+  }
+
+
+// create method just to trigger a release through Travis.
+  addSubcontract(data: any): Promise<void> {
+    const collection = this.afs.collection('subcontracts');
+    const id = data.id;
+    return collection.doc(id).set(data);
+  }
 
 }
+
+
